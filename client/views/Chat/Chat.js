a308a2aa2d87f6e8b416eded2791ba7b80dcc98b
flolas <flolas@alumnos.uai.cl>
2016-03-30 16:35:06 -0300

    Slider fixed

--- client/views/Chat/Chat.js
+++ client/views/Chat/Chat.js
@@ -1,11 +1,10 @@
 Meteor.subscribe('Chats');
 Session.set('slider', parseInt(moment(new Date("01-09-2011 00:00")).format('X')));
-
 function renderSlider() {
     slider = this.$("#slider").noUiSlider({
       range: {
-        'min': parseInt(moment(new Date("01-09-2011 00:00")).format('X')),
-        'max': parseInt(moment(new Date("01-10-2011 00:00")).format('X'))
+        'min': Session.get('sliderMin'),
+        'max': Session.get('sliderMax')
       },
       animate: true,
       start: Session.get('slider'),
@@ -17,7 +16,9 @@ function renderSlider() {
       Session.set('slider', val);
   });
 }
+Tracker.autorun(function () {
 Template.Chat.onRendered(renderSlider);
+});
 
 Template['Chat'].helpers({
     slider: function () {
@@ -30,6 +31,8 @@ Template['Chat'].helpers({
       id = Router.current().params._id;
       Bubbles = new Mongo.Collection(null);
       chats = Chats.findOne({ _id : id })
+      Session.set('sliderMin', parseInt(moment(_.first(_.pluck(chats.bubbles, 'sentAt'))).format('X')));
+      Session.set('sliderMax', parseInt(moment(_.last(_.pluck(chats.bubbles, 'sentAt'))).format('X')));
       for (var i = 0; i < chats.bubbles.length; i++) {
         Bubbles.insert(chats.bubbles[i]);
       }
@@ -40,16 +43,6 @@ Template['Chat'].helpers({
                           });
     }
 });
- /*Chats.findOne({
-                _id : Router.current().params._id,
-                bubbles: {
-                            $filter: {
-                               input: "$bubbles",
-                               as: "bubbles",
-                               cond: { $lte: [ "$$bubbles.sentAt", new Date("01-09-2011 01:00") ] }
-                            }
-                          }
-              });*/
 
 Template['Chat'].events({
   'click .play' : function(e) {
